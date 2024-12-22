720406f56a7344847acd901f2177765debf81b8a
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2024-12-22 22:05:08 +0530

    first commit of hello-world code

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract HelloWorld{
+    function getValue() public pure returns (string memory){
+            return "Hello world";
+    }
+}
