d58bc4747bfd3ae8c5a5d97d40541b38ee464635
Mishal Turkane <95625543+mishalturkane@users.noreply.github.com>
2025-01-08 22:50:06 +0530

    Update ethereum name service contract

--- ENS.sol
+++ ENS.sol
@@ -1,27 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  0.8.26;
+pragma solidity 0.8.26;
 
-contract ENS{
-      mapping (address => string) public users;
+contract ENS {
+    mapping(address => string) public users;
 
-      function signUp(string memory userName)public {
-            users[msg.sender]=userName;
-      }
+    function signUp(string memory userName) public {
+        require(bytes(users[msg.sender]).length == 0, "Username already set");
+        require(bytes(userName).length > 0, "Username cannot be empty");
+        users[msg.sender] = userName;
+    }
 
-      function getName() public view returns (string memory){
+    function getName() public view returns (string memory) {
+        require(bytes(users[msg.sender]).length > 0, "No username found");
         return users[msg.sender];
+    }
 
-      }
-      function getUserAddress()public view returns (address){
-        return  msg.sender;
-      }
-
-      function getUserAddressByName(address userAddress) public view returns (string memory){
-            if(msg.sender==userAddress){
-                return users[msg.sender];
-            }
-            else {
-                return "no user";
-            }
-      }
+    function getUserAddressByName(address userAddress) public view returns (string memory) {
+        if (bytes(users[userAddress]).length > 0) {
+            return users[userAddress];
+        } else {
+            return "No user found";
+        }
+    }
 }
